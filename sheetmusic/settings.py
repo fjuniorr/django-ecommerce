8e9f522049d4a6927f34f27d5af9865d08c8de65
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-10-20 11:31:50 -0300

    django-admin startproject sheetmusic .

--- sheetmusic/settings.py
+++ sheetmusic/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-4(rlw8u^rhl+d%_w2pa(yk0(rqdfi#_6d+o$!peuk$0&+@is'
+SECRET_KEY = 'django-insecure-&j0jxkl(zm!gbm(bg%62j6d&s!jluy5fzls@s4-u%)3%*db3z+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
