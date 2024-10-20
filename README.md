995d66cd6d3eeaeb89722b6507d5cb3ffd355442
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-10-20 12:04:39 -0300

    postgres is working

---
+++ README.md
@@ -0,0 +1,9 @@
+# Docs
+
+Make sure the database and redis are running by running the following commands:
+
+```
+docker compose up
+psql -h localhost  -U postgres
+redis-cli -h localhost ping
+```
--- sheetmusic/settings.py
+++ sheetmusic/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sheetmusic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  # or the IP address of your Docker host
+        'PORT': '5432',
     }
 }
 
